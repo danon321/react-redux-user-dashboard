9729d49e0b3310cdbab3ff9c3e6b54dbb70f7aef
Daniel <44358467+danon321@users.noreply.github.com>
2023-05-17 14:15:38 +0200

    Add delete button and modal

---
+++ src/components/User/UserDeleteButton.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import { Box, Button, ButtonGroup, Modal } from "@mui/material";
+
+interface UserDeleteButtonProps {
+  userId: number;
+  userName: string;
+}
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+function UserDeleteButton(props: UserDeleteButtonProps) {
+  const { userId, userName } = props;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleOpen} variant="contained" color="error">
+        Delete
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <h2 id="parent-modal-title">Delete user</h2>
+          <p id="parent-modal-description">Czy aby na pewno chcesz usunąć użytwkonika {userName}</p>
+
+          <ButtonGroup disableElevation variant="contained" aria-label="Disabled elevation buttons">
+            <Button variant="contained" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="contained" color="error">
+              Delete
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+export default UserDeleteButton;
--- src/components/User/UserItem.tsx
+++ src/components/User/UserItem.tsx
@@ -1,5 +1,18 @@
-function UserItem() {
-  return <div>User Item</div>;
+import { User } from "../../types/User";
+import UserDeleteButton from "./UserDeleteButton";
+
+interface UserItemProps {
+  user: User;
+}
+
+function UserItem(props: UserItemProps) {
+  const { user } = props;
+  return (
+    <div>
+      ID: {user.id} Name: {user.name}
+      <UserDeleteButton userId={user.id} userName={user.name} />
+    </div>
+  );
 }
 
 export default UserItem;
