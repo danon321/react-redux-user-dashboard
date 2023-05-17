43cb83a8097fc3661e453925bcde58f45ba00aac
Daniel <44358467+danon321@users.noreply.github.com>
2023-05-17 11:18:50 +0200

    Show information about loading and errors.

--- src/components/User/UserList.tsx
+++ src/components/User/UserList.tsx
@@ -3,21 +3,25 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { getUsers } from "../../store/user/api/getUsers";
 import { getUserState } from "../../store/user/userSlice";
+import { UserLoadStatus } from "../../types/User";
+import UserItem from "./UserItem";
 
 function UserList() {
   const dispatch = useAppDispatch();
   const { users, error, status } = useAppSelector(getUserState);
 
   useEffect(() => {
-    dispatch(getUsers());
+    if (status === UserLoadStatus.idle) dispatch(getUsers());
   }, []);
+
+  if (status === UserLoadStatus.failed) return <div>{error}</div>;
+
+  if (status === UserLoadStatus.loading) return <div>Loading data...</div>;
 
   return (
     <div>
       {users.map((user) => (
-        <div key={user.id}>
-          Id: {user.id} Name: {user.name}
-        </div>
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
